94bc3b56918c4080ef87cd4db7abfee23c1f4f53
syuusekizou <croissant.craft.boss@gmail.com>
2022-01-31 23:49:43 +0900

    ファイルを追加

---
+++ SortableTable.js
@@ -0,0 +1,41 @@
+const sortObj = new SortableTable();
+
+//並び変えるテーブルを設定 
+sortObj.setTable(document.querySelector('#my-table1'));
+
+//id=urlの項目にリンクを設定
+sortObj.setCellRenderer((col, row) => {
+    const colValue = row[col.id];
+    // cell-is-a-header
+    if (col.isHeader) {
+        if (typeof colValue !== 'undefined') {
+            return `<th>${colValue}</th>`;
+        }
+        return '<th></th>';
+    }
+    // cell-is-not-a-header
+    if (typeof colValue !== 'undefined') {
+        if (col.id === 'URL') {
+            return `<td><a href="${colValue}" target="_blank">${colValue}</a></td>`;
+        }
+        return `<td>${colValue}</td>`;
+    }
+    return '<td></td>';
+});
+
+//データ設定
+let items = [];
+let index = 1;
+singableList.forEach(x => items.push({
+    id: index++,
+    songTitles: x[0],
+    URL:x[1],
+    title:x[2]
+}));
+sortObj.setData(items);
+
+// button handlers
+document.querySelector('#btnReset')
+    .addEventListener('click', () => {
+        sortObj.resetData();
+    });
