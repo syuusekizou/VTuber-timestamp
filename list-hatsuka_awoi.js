e9c8757810c396e2d485f7fbc3026551ec0a603c
syuusekizou <croissant.craft.boss@gmail.com>
2022-09-25 20:05:11 +0900

    薄荷あをいを追加

---
+++ list-hatsuka_awoi.js
@@ -0,0 +1,10 @@
+const singableList = [
+  ['メリュー', 'https://youtu.be/2KCcQ87ZaTc&t=0490s', '2022/09/23', 'はじめての歌枠です！【#新人Vtuber / 歌枠】'],
+  ['炎', 'https://youtu.be/2KCcQ87ZaTc&t=0935s', '2022/09/23', 'はじめての歌枠です！【#新人Vtuber / 歌枠】'],
+  ['怪物', 'https://youtu.be/2KCcQ87ZaTc&t=1437s', '2022/09/23', 'はじめての歌枠です！【#新人Vtuber / 歌枠】'],
+  ['メルト', 'https://youtu.be/2KCcQ87ZaTc&t=2092s', '2022/09/23', 'はじめての歌枠です！【#新人Vtuber / 歌枠】'],
+  ['ガーネット', 'https://youtu.be/2KCcQ87ZaTc&t=2931s', '2022/09/23', 'はじめての歌枠です！【#新人Vtuber / 歌枠】'],
+  ['少女レイ', 'https://youtu.be/Ls4Shu4H_Tc&t=0238s', '2022/09/24', '【#歌枠】君と夏の終わり夢の始まり新人V歌リレー【 新人Vtuber / 薄荷あをい 】- singing stream -'],
+  ['新時代', 'https://youtu.be/Ls4Shu4H_Tc&t=0616s', '2022/09/24', '【#歌枠】君と夏の終わり夢の始まり新人V歌リレー【 新人Vtuber / 薄荷あをい 】- singing stream -'],
+  ['花に亡霊', 'https://youtu.be/Ls4Shu4H_Tc&t=0927s', '2022/09/24', '【#歌枠】君と夏の終わり夢の始まり新人V歌リレー【 新人Vtuber / 薄荷あをい 】- singing stream -'],
+];
